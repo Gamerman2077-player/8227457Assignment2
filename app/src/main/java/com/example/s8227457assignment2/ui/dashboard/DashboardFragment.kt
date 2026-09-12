4517bd4d90083273848ee30cdee5b2fed1fce5be
Sohil Khan <s8227457@live.vu.edu.au>
2026-09-12 23:10:02 +1000

    Clean up UI resources and navigation

---
+++ app/src/main/java/com/example/s8227457assignment2/ui/dashboard/DashboardFragment.kt
@@ -0,0 +1,19 @@
+package com.example.s8227457assignment2.ui.dashboard
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.s8227457assignment2.R
+
+class DashboardFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(R.layout.fragment_dashboard, container, false)
+    }
+}
---
+++ app/src/main/java/com/example/s8227457assignment2/ui/login/LoginFragment.kt
@@ -0,0 +1,19 @@
+package com.example.s8227457assignment2.ui.login
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.s8227457assignment2.R
+
+class LoginFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(R.layout.fragment_login, container, false)
+    }
+}
